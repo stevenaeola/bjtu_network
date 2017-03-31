f68100285e11015bf223bec51a8a7b697c40356e
dcs0spb <s.p.bradley@durham.ac.uk>
2017-03-31 11:56:02 +0800

    Load and save ClientPC, using exceptions, test fails but will fix equals method

--- ClientPC.java
+++ ClientPC.java
@@ -1,30 +1,62 @@
+import java.io.*;
+import java.util.Scanner;
 
 public class ClientPC implements DeviceInterface
 {
-   private IPAddr ip_address;
-   private String mac_address;
-   private String manufacturer;
-   private String model;
-   
-   public ClientPC(String mac_address){
-       this.mac_address = mac_address;
-       manufacturer ="";
-       model = "";
-   }
-    
-   public IPAddr getIPaddress(){
-       return ip_address;
+    private IPAddr ip_address;
+    private String mac_address;
+    private String manufacturer;
+    private String model;
+
+    public ClientPC(String mac_address, String manufacturer, String model){
+        this.mac_address = mac_address;
+        this.manufacturer = manufacturer;
+        this.model = model;
     }
     
+    /**
+     * creates an object based on a previusly saved file
+     */
+    public ClientPC(String fname) throws FileNotFoundException{
+        Scanner sc = new Scanner(new File(fname));
+        manufacturer = sc.nextLine();
+        model = sc.nextLine();
+        ip_address = new IPAddr(sc.nextLine());
+        mac_address = sc.nextLine();
+    }
+
+    public IPAddr getIPaddress(){
+        return ip_address;
+    }
+
     public String getMacAddress(){
-       return mac_address;
+        return mac_address;
     }
-    
+
     public void setIPAddress(IPAddr ip_address){
         this.ip_address = ip_address;
     }
-    
+
     public String toString(){
-        return manufacturer + " " + model + "\n" + ip_address.toString() + " " + mac_address;
+        return manufacturer + "\n" + model + "\n" + ip_address.toString() + "\n" + mac_address;
+    }
+
+    public void saveToFile(String fname){
+
+        try{
+            FileWriter writer = new FileWriter(fname);
+            writer.write(toString());
+            writer.close();
+        }
+        catch(IOException e){
+            System.out.println("Ooops " + e);
+        }
+    }
+
+    public void saveToFile2(String fname) throws IOException{
+            FileWriter writer = new FileWriter(fname);
+            writer.write(toString());
+            writer.close();
+
     }
 }
--- ClientPCTest.java
+++ ClientPCTest.java
@@ -12,6 +12,8 @@ import org.junit.Test;
  */
 public class ClientPCTest
 {
+    private ClientPC clientPC1;
+    
     /**
      * Default constructor for test class ClientPCTest
      */
@@ -27,7 +29,9 @@ public class ClientPCTest
     @Before
     public void setUp()
     {
+        clientPC1 = new ClientPC("PC", "Dell", "123456");
     }
+    
 
     /**
      * Tears down the test fixture.
@@ -43,7 +47,6 @@ public class ClientPCTest
     public void ip_toString()
     {
         IPAddr iPAddr1 = new IPAddr(129, 234, 0, 2);
-        ClientPC clientPC1 = new ClientPC("1234356");
         clientPC1.setIPAddress(iPAddr1);
         assertEquals(" \n129.234.0.2 1234356", clientPC1.toString());
     }
@@ -51,9 +54,36 @@ public class ClientPCTest
     @Test
     public void setIP(){
         IPAddr iPAddr1 = new IPAddr(129, 234, 0, 2);
-        ClientPC clientPC1 = new ClientPC("1234356");
         clientPC1.setIPAddress(iPAddr1);
         assertEquals(clientPC1.getIPaddress(), iPAddr1);
+
+    }
+
+    @Test
+    public void save_to_file(){
+        IPAddr iPAddr1 = new IPAddr(129, 234, 0, 2);
+        clientPC1.setIPAddress(iPAddr1);
+        clientPC1.saveToFile("PC1.txt");
+
+    }
+
+    @Test
+    public void save_to_file_broken() throws java.io.IOException{
+        IPAddr iPAddr1 = new IPAddr(129, 234, 0, 2);
+        clientPC1.setIPAddress(iPAddr1);
+        clientPC1.saveToFile2("broken/PC1.txt");
+
+    }
+    
+        @Test
+    public void save_and_load() throws Exception {
+        IPAddr iPAddr1 = new IPAddr(129, 234, 0, 2);
+        clientPC1.setIPAddress(iPAddr1);
+        clientPC1.saveToFile("PC1.txt");
+        
+        ClientPC clientPC2 = new ClientPC("PC1.txt");
+        
+        assertEquals(clientPC1, clientPC2);
 
     }
 }
